7dec83c01525cfd3c9362e41823421253400646b
Gert Mertes <hey@gertmertes.be>
2024-06-26 12:41:19 +0000

    Use anemoi.utils.config

--- src/anemoi/training/diagnostics/mlflow/auth.py
+++ src/anemoi/training/diagnostics/mlflow/auth.py
@@ -1,53 +1,34 @@
-import json
 import logging
-import os
 import time
 from getpass import getpass
 
 import requests
+from anemoi.utils.config import load_config
+from anemoi.utils.config import save_config
 from requests.exceptions import HTTPError
 
 LOG = logging.getLogger(__name__)
 
-REFRESH_TOKEN_EXPIRE = 29  # days
-CONFIG_PATH = os.path.expanduser("~/.config/anemoi")
-CONFIG_FILE = os.path.join(CONFIG_PATH, "mlflow-auth.json")
-
-
-def get_config():
-    try:
-        with open(CONFIG_FILE, "r") as file:
-            config = json.load(file)
-        return config
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}
-
-
-def save_config(refresh_token):
-    if not os.path.exists(CONFIG_PATH):
-        os.makedirs(CONFIG_PATH)
-
-    refresh_expires = time.time() + (REFRESH_TOKEN_EXPIRE * 24 * 60 * 60)
-    config = {
-        "refresh_token": refresh_token,
-        "refresh_expires": int(refresh_expires),
-    }
-
-    with open(CONFIG_FILE, "w") as file:
-        json.dump(config, file)
-
 
 class TokenAuthenticator:
-    def __init__(self, uri: str = "https://mlflow-test.ecmwf.int", enabled=True):
+    def __init__(
+        self,
+        uri="https://mlflow-test.ecmwf.int",
+        refresh_expire_days=29,
+        enabled=True,
+    ):
         self.uri = uri
+        self.refresh_expire_days = refresh_expire_days
         self.enabled = enabled
+
+        self.config_file = "mlflow-token.json"
         self.refresh_token = None
         self.auth_token = None
         self.auth_expires = 0
 
     def login(self):
 
-        config = get_config()
+        config = load_config(self.config_file)
 
         refresh_token = config.get("refresh_token")
         refresh_expires = config.get("refresh_expires", 0)
@@ -61,10 +42,18 @@ class TokenAuthenticator:
 
         if new_refresh_token:
             self.refresh_token = new_refresh_token
-            save_config(new_refresh_token)
+            self._save_config(new_refresh_token)
             LOG.info("Successfully authenticated with MLflow. Happy logging!")
         else:
             raise ValueError("No refresh token received.")
+
+    def _save_config(self, refresh_token: str):
+        refresh_expires = time.time() + (self.refresh_expire_days * 24 * 60 * 60)
+        config = {
+            "refresh_token": refresh_token,
+            "refresh_expires": int(refresh_expires),
+        }
+        save_config(self.config_file, config)
 
     def _credential_login(self, username: str, password: str):
         payload = {"username": username, "password": password}
