9e9c1b53f862b85bd357cf0ce30d3aad43147e3d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2024-06-28 16:55:38 +0100

    Better implementation of overrides

--- src/anemoi/training/commands/train.py
+++ src/anemoi/training/commands/train.py
@@ -16,6 +16,7 @@ import re
 
 import hydra
 from anemoi.utils.config import config_path
+from hydra.errors import ConfigCompositionException
 from omegaconf import OmegaConf
 
 from . import Command
@@ -43,6 +44,76 @@ override_regex = re.compile(
 )
 
 
+def apply_delete_override(cfg, dotkey, value, parent, key, value_given):
+
+    any_value = object()
+
+    if value_given:
+        value_given = value
+    else:
+        value_given = any_value
+
+    value = OmegaConf.select(cfg, dotkey, throw_on_missing=False)
+    if value_given is not any_value and value != value_given:
+        raise ConfigCompositionException(
+            f"Key '{dotkey}' with value '{value}' does not match the value '{value_given}' in the override"
+        )
+
+    try:
+        # Allow 'del'
+        OmegaConf.set_struct(cfg, False)
+        if key is None:
+            # Top level key
+            del cfg[parent]
+        else:
+            subtree = OmegaConf.select(cfg, parent)
+            del subtree[key]
+    finally:
+        OmegaConf.set_struct(cfg, True)
+
+
+def apply_add_override_force(cfg, dotkey, value, parent, key):
+    OmegaConf.update(cfg, dotkey, value, merge=True, force_add=True)
+
+
+def apply_add_override(cfg, dotkey, value, parent, key):
+    current = OmegaConf.select(cfg, dotkey, throw_on_missing=False)
+    if current is not None:
+        raise ConfigCompositionException(f"Cannot add key '{dotkey}' because it already exists, use '++' to force add")
+
+    OmegaConf.update(cfg, dotkey, value, merge=True, force_add=True)
+
+
+def apply_assign_override(cfg, dotkey, value, parent, key):
+    OmegaConf.update(cfg, dotkey, value, merge=True)
+
+
+def parse_override(override, n):
+    dotkey = override[n:]
+    parsed = OmegaConf.from_dotlist([dotkey])
+    dotkey = dotkey.split("=")[0]
+    value = OmegaConf.select(parsed, dotkey)
+
+    if "." in dotkey:
+        parent, key = dotkey.rsplit(".", 1)
+        return dotkey, value, parent, key
+    else:
+        return dotkey, value, dotkey, None
+
+
+def apply_override(cfg, override):
+    if override.startswith("~"):
+        return apply_delete_override(cfg, *parse_override(override, 1), value_given="=" in override)
+
+    if override.startswith("++"):
+        return apply_add_override_force(cfg, *parse_override(override, 2))
+
+    if override.startswith("+"):
+        return apply_add_override(cfg, *parse_override(override, 1))
+
+    return apply_assign_override(cfg, *parse_override(override, 0))
+
+
 class Train(Command):
 
     def add_arguments(self, command_parser):
@@ -68,15 +139,14 @@ class Train(Command):
 
         hydra.initialize(config_path="../config", version_base=None)
 
-        cfg = hydra.compose(config_name="config")  # , overrides=overrides)
-        print(cfg)
+        cfg = hydra.compose(config_name="config")
 
         # Add user config
         user_config = config_path("train.yaml")
 
         if os.path.exists(user_config):
             LOGGER.info(f"Loading config {user_config}")
-            cfg = OmegaConf.merge(cfg, OmegaConf.load(user_config))
+            cfg = OmegaConf.merge(cfg, OmegaConf.load(user_config, resolve=True))
 
         # Add extra config files specified in the command line
 
@@ -85,10 +155,15 @@ class Train(Command):
             cfg = OmegaConf.merge(cfg, OmegaConf.load(config))
 
         # We need to reapply the overrides
-        # This does not support overrides with a prefix
-        cfg = OmegaConf.merge(cfg, OmegaConf.from_dotlist(overrides))
+        # OmegaConf do not implement the prefix logic, this is done by hydra
+        for override in overrides:
+            LOGGER.info(f"Applying override {override}")
+            apply_override(cfg, override)
 
-        print(json.dumps(OmegaConf.to_container(cfg, resolve=True), indent=4))
+        # Resolve the config
+        OmegaConf.resolve(cfg)
+
+        print(json.dumps(OmegaConf.to_container(cfg), indent=4))
 
         # AIFSTrainer(cfg).train()
 
