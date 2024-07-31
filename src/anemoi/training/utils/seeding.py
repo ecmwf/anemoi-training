5d9769d8809532e28dcaca1aa2978bc1500bad44
Gert Mertes <gert.mertes@ecmwf.int>
2024-07-31 15:22:37 +0000

    Rename AIFS_BASE_SEED -> ANEMOI_BASE_SEED

--- src/anemoi/training/utils/seeding.py
+++ src/anemoi/training/utils/seeding.py
@@ -1,10 +1,10 @@
 import os
 
 
-def get_base_seed(env_var_list=("AIFS_BASE_SEED", "SLURM_JOB_ID")) -> int:
+def get_base_seed(env_var_list=("ANEMOI_BASE_SEED", "SLURM_JOB_ID")) -> int:
     """Gets the base seed from the environment variables.
 
-    Option to manually set a seed via export AIFS_BASE_SEED=xxx in job script
+    Option to manually set a seed via export ANEMOI_BASE_SEED=xxx in job script
     """
     base_seed = None
     for env_var in env_var_list:
