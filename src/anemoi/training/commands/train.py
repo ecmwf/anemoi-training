4cf23f67540cf047879c1a92d660a31035f53633
Jesper Dramsch <jesper.dramsch@ecmwf.int>
2024-08-02 13:50:10 +0000

    feature: training subcommand

--- src/anemoi/training/commands/train.py
+++ src/anemoi/training/commands/train.py
@@ -8,88 +8,32 @@
 # nor does it submit to any jurisdiction.
 #
 
-
-import json
 import logging
-import os
-import re
+import sys
 
-import hydra
-from anemoi.utils.config import config_path
-from omegaconf import OmegaConf
+from anemoi.training.train.train import main as anemoi_train
 
 from . import Command
 
 LOGGER = logging.getLogger(__name__)
 
-# https://hydra.cc/docs/advanced/override_grammar/basic/
-
-override_regex = re.compile(
-    r"""
-        ^
-        (
-            (\w+)([/@:\.]\w+)*  # key
-            =                   # assignment
-            (.*)                # value
-        )
-        $
-    """,
-    re.VERBOSE,
-)
-
 
 class Train(Command):
+    accept_unknown_args = True
 
-    def add_arguments(self, command_parser):
-        command_parser.add_argument(
-            "config",
-            nargs="*",
-            type=str,
-            help="A list yaml files to load or a list of overrides to apply",
-        )
+    def add_arguments(self, parser):
+        return parser
 
-    def run(self, args):
+    def run(self, args, unknown_args=None):
+        del args
 
-        configs = []
-        overrides = []
+        if unknown_args is not None:
+            sys.argv = [sys.argv[0]] + unknown_args
+        else:
+            sys.argv = [sys.argv[0]]
 
-        for config in args.config:
-            if override_regex.match(config):
-                overrides.append(config)
-            elif config.endswith(".yaml") or config.endswith(".yml"):
-                configs.append(config)
-            else:
-                raise ValueError(f"Invalid config '{config}'. It must be a yaml file or an override")
-
-        # We could apply the overrides here. To be tested
-        hydra.initialize(config_path="../config", version_base=None)
-
-        cfg = hydra.compose(config_name="config")
-
-        # Add user config
-        user_config = config_path("training.yaml")
-
-        if os.path.exists(user_config):
-            LOGGER.info(f"Loading config {user_config}")
-            cfg = OmegaConf.merge(cfg, OmegaConf.load(user_config))
-
-        # Add extra config files specified in the command line
-
-        for config in configs:
-            LOGGER.info(f"Loading config {config}")
-            cfg = OmegaConf.merge(cfg, OmegaConf.load(config))
-
-        # Apply overrides
-        # OmegaConf do not implement the prefix logic, this is done by hydra
-        # If needed, the logic can be implemented here (look in the git history for an example)
-        OmegaConf.merge(cfg, OmegaConf.from_dotlist(overrides))
-
-        # Resolve the config
-        OmegaConf.resolve(cfg)
-
-        print(json.dumps(OmegaConf.to_container(cfg), indent=4))
-
-        # AIFSTrainer(cfg).train()
+        LOGGER.info(f"Running anemoi training command with overrides: {sys.argv[1:]}")
+        anemoi_train()
 
 
 command = Train
