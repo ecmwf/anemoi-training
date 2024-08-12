88a714e402920adc8f277e4fca3b175d1d7f2abf
mc4117 <mariana.clare@ecmwf.int>
2024-08-12 17:28:30 +0200

    fix: anemoi search path hierarchy

--- src/hydra_plugins/anemoi_searchpath/anemoi_searchpath_plugin.py
+++ src/hydra_plugins/anemoi_searchpath/anemoi_searchpath_plugin.py
@@ -1,50 +1,60 @@
+import logging
 import os
-from logging import getLogger
 from pathlib import Path
 
 from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
-LOGGER = getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
-class AnemoiHomeSearchPathPlugin(SearchPathPlugin):
+class AnemoiSearchPathPlugin(SearchPathPlugin):
     """Prepend the Anemoi home directory to the hydra searchpath."""
 
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        anemoi_home_path = Path(Path.home(), ".config", "anemoi", "training", "config")
-        if anemoi_home_path.exists():
-            search_path.prepend(
-                provider="anemoi-home-searchpath-plugin",
-                path=str(anemoi_home_path),
-            )
-            LOGGER.info(f"Prepending {anemoi_home_path} to the search path.")
-            LOGGER.debug(f"Search path is now: {search_path}")
+        """Prepend the Anemoi directories to the hydra searchpath.
 
+        This builds the hierarchy of the search path by prepending the Anemoi
+        directories to the search path. The hierarchy is as follows in decreasing priority:
+        - Current working directory
+        - Anemoi Config Env directory
+        - Anemoi Home directory
 
-class AnemoiEnvSearchPathPlugin(SearchPathPlugin):
-    """Prepend the path env ANEMOI_CONFIG_PATH to hydra searchpath."""
+        Parameters
+        ----------
+        search_path : ConfigSearchPath
+            Hydra ConfigSearchPath object.
+        """
+        for suffix in ("", "config"):
+            anemoi_home_path = Path(Path.home(), ".config", "anemoi", "training", suffix)
+            if anemoi_home_path.exists() and not Path(anemoi_home_path, "config").exists():
+                search_path.prepend(
+                    provider="anemoi-home-searchpath-plugin",
+                    path=str(anemoi_home_path),
+                )
+                LOGGER.info(f"Prepending Anemoi Home ({anemoi_home_path}) to the search path.")
+                LOGGER.debug(f"Search path is now: {search_path}")
 
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        anemoi_config_path = os.getenv("ANEMOI_CONFIG_PATH")
-        if anemoi_config_path is not None and anemoi_config_path.exists():
-            search_path.prepend(
-                provider="anemoi-env-searchpath-plugin",
-                path=str(anemoi_config_path),
-            )
-            LOGGER.info(f"Appending {anemoi_config_path} to the search path.")
-            LOGGER.debug(f"Search path is now: {search_path}")
+        for suffix in ("", "config"):
+            env_anemoi_config_path = os.getenv("ANEMOI_CONFIG_PATH")
+            if env_anemoi_config_path is None:
+                return
+            anemoi_config_path = Path(env_anemoi_config_path)
+            if anemoi_config_path.exists() and not Path(anemoi_config_path, "config").exists():
+                search_path.prepend(
+                    provider="anemoi-env-searchpath-plugin",
+                    path=str(anemoi_config_path),
+                )
+                LOGGER.info(f"Prepending Anemoi Config Env ({anemoi_config_path}) to the search path.")
+                LOGGER.debug(f"Search path is now: {search_path}")
 
-
-class UserCWDSearchPathPlugin(SearchPathPlugin):
-    """Prepend the current working directory to the hydra search path."""
-
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        cwd_path = Path(Path.cwd(), "config")
-        if cwd_path.exists():
-            search_path.prepend(
-                provider="anemoi-env-searchpath-plugin",
-                path=str(cwd_path),
-            )
-            LOGGER.info(f"Appending {cwd_path} to the search path.")
-            LOGGER.debug(f"Search path is now: {search_path}")
+        for suffix in ("", "config"):
+            cwd_path = Path.cwd() / suffix
+            if cwd_path.exists() and not Path(cwd_path, "config").exists():
+                search_path.prepend(
+                    provider="anemoi-cwd-searchpath-plugin",
+                    path=str(cwd_path),
+                )
+                LOGGER.info(f"Prepending current user directory ({cwd_path}) to the search path. ")
+                LOGGER.debug(f"Search path is now: {search_path}")
+        LOGGER.info(f"Search path is now: {search_path}")
