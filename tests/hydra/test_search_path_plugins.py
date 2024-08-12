9357769dc427909e052c831a3306ae1e546a50a7
mc4117 <mariana.clare@ecmwf.int>
2024-08-12 17:28:30 +0200

    fix: searchpath plugin

--- tests/hydra/test_search_path_plugins.py
+++ tests/hydra/test_search_path_plugins.py
@@ -1,26 +1,21 @@
+# (C) Copyright 2024 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
 from hydra import initialize
 from hydra.core.global_hydra import GlobalHydra
 from hydra.core.plugins import Plugins
 from hydra.plugins.search_path_plugin import SearchPathPlugin
-
-from hydra_plugins.anemoi_searchpath.anemoi_searchpath_plugin import AnemoiEnvSearchPathPlugin
-from hydra_plugins.anemoi_searchpath.anemoi_searchpath_plugin import AnemoiHomeSearchPathPlugin
-from hydra_plugins.anemoi_searchpath.anemoi_searchpath_plugin import UserCWDSearchPathPlugin
+from hydra_plugins.anemoi_searchpath.anemoi_searchpath_plugin import AnemoiSearchPathPlugin
 
 
 def test_anemoi_home_searchpath_discovery() -> None:
     # Tests that this plugin can be discovered via the plugins subsystem when looking at all Plugins
-    assert AnemoiHomeSearchPathPlugin.__name__ in [x.__name__ for x in Plugins.instance().discover(SearchPathPlugin)]
-
-
-def test_anemoi_env_searchpath_discovery() -> None:
-    # Tests that this plugin can be discovered via the plugins subsystem when looking at all Plugins
-    assert AnemoiEnvSearchPathPlugin.__name__ in [x.__name__ for x in Plugins.instance().discover(SearchPathPlugin)]
-
-
-def test_anemoi_cwd_searchpath_discovery() -> None:
-    # Tests that this plugin can be discovered via the plugins subsystem when looking at all Plugins
-    assert UserCWDSearchPathPlugin.__name__ in [x.__name__ for x in Plugins.instance().discover(SearchPathPlugin)]
+    assert AnemoiSearchPathPlugin.__name__ in [x.__name__ for x in Plugins.instance().discover(SearchPathPlugin)]
 
 
 def test_config_installed() -> None:
