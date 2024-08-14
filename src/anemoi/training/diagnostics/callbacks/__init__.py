f187c5b5a357408472228cfac638ca0a29117017
sahahner <hahner.sa@gmail.com>
2024-08-14 13:41:30 +0000

    fix: fix from develop

--- src/anemoi/training/diagnostics/callbacks/__init__.py
+++ src/anemoi/training/diagnostics/callbacks/__init__.py
@@ -452,6 +452,7 @@ class PlotLoss(BasePlotCallback):
         pl_module: pl.Lightning_module,
         outputs: list[torch.Tensor],
         batch: torch.Tensor,
+        batch_idx: int,
         epoch: int,
     ) -> None:
         logger = trainer.logger
