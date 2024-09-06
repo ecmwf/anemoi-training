bugfixes: logic relating to feature weighting, batch_idx not used in RolloutEval, incorrect naming of val_metric_ranges

src/anemoi/training/diagnostics/callbacks/__init__.py
@@ -194,9 +194,10 @@ class RolloutEval(Callback):
         self,
         pl_module: pl.LightningModule,
         batch: torch.Tensor,
+        batch_idx: int,
     ) -> None:
         with torch.no_grad():
-            loss, metrics, _ = pl_module._step(batch, validation_mode=True, in_place_proc=False)
+            loss, metrics, _ = pl_module._step(batch, validation_mode=True, in_place_proc=False, batch_idx=batch_idx)
 
             self._log(pl_module, loss, metrics, batch.shape[0])
 
@@ -245,7 +246,7 @@ class RolloutEval(Callback):
             context = torch.autocast(device_type=batch.device.type, dtype=dtype) if dtype is not None else nullcontext()
 
             with context:
-                self._eval(pl_module, batch)
+                self._eval(pl_module, batch, batch_idx)
 
 
 class GraphTrainableFeaturesPlot(BasePlotCallback):

src/anemoi/training/losses/mse.py
@@ -16,7 +16,7 @@ from torch import nn
 from typing import Optional
 LOGGER = logging.getLogger(__name__)
 
-#TODO (rilwan-ade): remove the data_variances/tendency_variances and replace the name with feature_weights - then change loss_
+
 class WeightedMSELoss(nn.Module):
     """Latitude-weighted MSE loss."""
 
@@ -70,8 +70,8 @@ class WeightedMSELoss(nn.Module):
 
         """
         out = torch.square(pred - target)
-
-        out = out * self.feature_weights
+        if feature_scaling:
+            out = out * self.feature_weights
 
         # Squash by last dimension
         if squash: