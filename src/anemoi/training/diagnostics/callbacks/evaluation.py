Fix asserrt calculation

src/anemoi/training/diagnostics/callbacks/evaluation.py
@@ -61,7 +61,7 @@ class RolloutEval(Callback):
 
         assert batch.shape[1] >= self.rollout + pl_module.multi_step, (
             "Batch length not sufficient for requested validation rollout length! "
-            f"Set `dataloader.validation_rollout` to at least {self.rollout + pl_module.multi_step}"
+            f"Set `dataloader.validation_rollout` to at least {max(self.rollout)}"
         )
 
         with torch.no_grad():