31431338fee0dcb1ef059a5568ce91720ab12e72
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2024-12-03 16:53:27 +0000

    fix: set read_group_size in grid_indices __init__

--- src/anemoi/training/data/datamodule.py
+++ src/anemoi/training/data/datamodule.py
@@ -83,7 +83,8 @@ class AnemoiDatasetsDataModule(pl.LightningDataModule):
 
     @cached_property
     def grid_indices(self) -> type[BaseGridIndices]:
-        grid_indices = instantiate(self.config.dataloader.grid_indices)
+        read_group_size = self.config.dataloader.get("read_group_size", self.config.hardware.num_gpus_per_model)
+        grid_indices = instantiate(self.config.dataloader.grid_indices, read_group_size=read_group_size)
         grid_indices.setup(self.graph_data)
         return
 
