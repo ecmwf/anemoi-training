bd320962e42446825d16e46db676e4f278f4a543
icedoom888 <alberto.pennino.8@gmail.com>
2024-12-06 11:24:34 +0100

    Completed Merge and code check

--- src/anemoi/training/train/train.py
+++ src/anemoi/training/train/train.py
@@ -34,12 +34,7 @@ from anemoi.training.diagnostics.logger import get_tensorboard_logger
 from anemoi.training.diagnostics.logger import get_wandb_logger
 from anemoi.training.distributed.strategy import DDPGroupStrategy
 from anemoi.training.train.forecaster import GraphForecaster
-
-<<<<<<< HEAD
 from anemoi.training.utils.checkpoint import freeze_submodule_by_name
-
-=======
->>>>>>> develop
 from anemoi.training.utils.checkpoint import transfer_learning_loading
 from anemoi.training.utils.jsonify import map_config_to_primitives
 from anemoi.training.utils.seeding import get_base_seed
